bfb01dfd1c5fe65697f7818f7f49874aa1549d3a
ruslantilovov2 <ruslantilovov2@gmail.com>
2023-12-19 22:38:40 +0500

    full

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+  {
+    name: "Mana-Manam",
+    artist: "Bass Music",
+    img: "music1",
+    src: "music1"
+  },
+  {
+    name: "Qizil ko'ylagim",
+    artist: "Rayhon va Hamdam Sobirov",
+    img: "music2",
+    src: "music2"
+  },
+  {
+    name: "Onamdan ustun emas sevganlarim",
+    artist: "Oxunjon Sadiyev",
+    img: "music-3",
+    src: "music3"
+  },
+  {
+    name: "Charchadim ona",
+    artist: "Shoxruh Ergashev",
+    img: "music4",
+    src: "music4"
+  },
+  {
+    name: "Sevardimda Sinfdoshimni",
+    artist: "Jasur Boyka",
+    img: "music5",
+    src: "music5"
+  },
+  {
+    name: "Qanday yashashni bilmaydi hech kim",
+    artist: "Konsta",
+    img: "music6",
+    src: "music6"
+  },
+];
